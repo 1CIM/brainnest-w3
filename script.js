4b3a5d538d6ab63fb293935b187988261db64111
kim_haaga <kim.haaga@live.se>
2022-12-03 15:20:46 +0100

    Add replay functionality

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ let humanScore = 0;
 let result = '';
 let rounds = 0;
 let ties = 0;
+let playedGames = 0;
+let wonGames = 0;
+let lostGames = 0;
 
 alert('Open the console and type game() and press the return key to play a best of 5 rock paper scissor game.');
 
@@ -14,13 +17,21 @@ const resetScore = () => {
   rounds = 0;
 };
 
+const resetAll = () => {
+  resetScore();
+  playedGames = 0;
+  wonGames = 0;
+  lostGames = 0;
+}
+
 const overView = () => {
-  console.log(`The game ended in ${rounds + ties} rounds. \nYou won ${humanScore}, \nLost ${pcScore} and \nTied ${ties} times. \nType game() to play again`);
+  console.log(`The game ended in ${rounds + ties} rounds. \nYou won ${humanScore}, \nLost ${pcScore} and \nTied ${ties} times.`);
+  console.log(`And you have now played ${playedGames > 1 ? 'games' : 'game'}.`);
 };
 
-const playRound = () => {
+function playRound() {
   const pcChoice = hands[Math.floor(Math.random() * hands.length)];
-  const humanChoice = prompt(`Please select rock, paper or scissor. There is ${5 - rounds} ${rounds === 4 ? 'round' : 'rounds'} left`);
+  const humanChoice = prompt(`Please select rock, paper or scissor. There is ${5 - rounds} ${rounds === 4 ? 'round' : 'rounds'} left`).toLowerCase();
 
   if (
     humanChoice === 'rock' ||
@@ -51,16 +62,38 @@ const playRound = () => {
   }
 };
 
-const game = () => {
+function replay() {
+  const playAgain = prompt(`Play again? Type 'Yes' if not type 'No'..`).toLowerCase();
+  if (playAgain === 'yes' || playAgain === 'no') {
+    if(playAgain === 'no') {
+      alert(`Okay bye! 
+      \nTou played ${playedGames} ${playedGames === 1 ? 'Game' : 'Games'}
+      \nYour final score was ${wonGames} ${wonGames === 1 ? 'Won game' : 'Won games'} 
+      \n and you lost ${lostGames} ${lostGames === 1 ? 'Game' : 'Games'}`);
+      resetAll();
+    } else {
+      game();
+    }
+  } else {
+    alert(`You need to type yes or no not ${playAgain}`);
+    replay();
+  }
+};
+
+function game() {
   do {
     playRound();
   } while (rounds < 5);
-
+  
   if (humanScore < pcScore) {
     console.log('GG EZ NOOB, PLZ UNINSTALL TRASH');
+    lostGames++;
   } else {
     console.log('WOW gz man you must be sooo proud you won... get out and get a life');
+    wonGames++;
   }
+  playedGames++;
   overView();
   resetScore();
+  replay();
 };
